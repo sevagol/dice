d95a4b9abbd9645c27a9f698ea2c63a059a35f56
Lev Tomatny <mastergefest@gmail.com>
2023-10-30 04:42:36 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 import WebApp from '@twa-dev/sdk';
 
 function App() {
-  const [started, setStarted] = useState(""); // Используем состояние для хранения значения
+  const [started, setStarted] = useState("");
+  const [ended, setEnded] = useState("");
+  const [duration, setDuration] = useState(""); // Состояние для хранения продолжительности времени
 
   useEffect(() => {
     WebApp.setHeaderColor("secondary_bg_color");
@@ -17,20 +19,41 @@ function App() {
       const formattedTime = currentTime.toLocaleTimeString();
       alert(`Track started at: ${formattedTime}`);
       WebApp.CloudStorage.setItem("started_at", formattedTime);
-      setStarted(formattedTime); // Обновляем значение в состоянии
+      setStarted(formattedTime);
     });
 
-    const key = "started_at"; // Ваш ключ для получения значения
+    const key = "started_at";
 
     WebApp.CloudStorage.getItem(key, (error, result) => {
       if (error) {
         console.error("Произошла ошибка при получении данных:", error);
       } else if (result) {
-        // Обновляем значение в состоянии
         setStarted(result);
       }
     });
   }, []);
+
+  // Обработчик для второго нажатия на MainButton
+  const handleSecondClick = () => {
+    const currentTime = new Date();
+    const formattedTime = currentTime.toLocaleTimeString();
+    alert(`Track ended at: ${formattedTime}`);
+    WebApp.CloudStorage.setItem("ended_at", formattedTime);
+    setEnded(formattedTime);
+
+    if (started && ended) {
+      // Вычисляем продолжительность времени
+      const startTime = new Date(started);
+      const endTime = new Date(ended);
+      const timeDiff = endTime - startTime;
+
+      // Преобразуем продолжительность времени в минуты (или другой формат, по вашему выбору)
+      const minutes = Math.floor(timeDiff / 60000); // 60000 миллисекунд в минуте
+
+      // Обновляем состояние с продолжительностью времени
+      setDuration(`${minutes} минут`);
+    }
+  };
 
   return (
     <>
@@ -41,8 +64,17 @@ function App() {
       </div>
       <h1>DICE Time Tracker</h1>
       <div className="card">
-        {started}
+        {started && ended && (
+          <p>
+            Начало: {started}
+            <br />
+            Конец: {ended}
+            <br />
+            Продолжительность: {duration}
+          </p>
+        )}
       </div>
+      <button onClick={handleSecondClick}>Завершить трек</button>
     </>
   );
 }
