b8f785ba3b1b8c55f1b5799bb639c10289236a40
Lev Tomatny <mastergefest@gmail.com>
2023-10-30 04:21:41 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import dicelogo from './assets/dice.png'
-import './App.css'
-
-import WebApp from '@twa-dev/sdk'
+import { useEffect, useState } from 'react';
+import dicelogo from './assets/dice.png';
+import './App.css';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
-  WebApp.setHeaderColor("secondary_bg_color")
+  const [started, setStarted] = useState(""); // Используем состояние для хранения значения
 
-  const mainbutton = WebApp.MainButton;
-  mainbutton.setText('START TRACK');
-  mainbutton.show();
-  mainbutton.onClick(() => {
-    const currentTime = new Date();
-    const formattedTime = currentTime.toLocaleTimeString();
-    alert(`Track started at: ${formattedTime}`);
-    WebApp.CloudStorage.setItem("started_at", formattedTime)
-  });
-  WebApp.CloudStorage.getItem("started_at")
+  useEffect(() => {
+    WebApp.setHeaderColor("secondary_bg_color");
 
+    const mainbutton = WebApp.MainButton;
+    mainbutton.setText('START TRACK');
+    mainbutton.show();
+    mainbutton.onClick(() => {
+      const currentTime = new Date();
+      const formattedTime = currentTime.toLocaleTimeString();
+      alert(`Track started at: ${formattedTime}`);
+      WebApp.CloudStorage.setItem("started_at", formattedTime);
+      setStarted(formattedTime); // Обновляем значение в состоянии
+    });
+
+    const key = "started_at"; // Ваш ключ для получения значения
+
+    WebApp.CloudStorage.getItem(key, (error, result) => {
+      if (error) {
+        console.error("Произошла ошибка при получении данных:", error);
+      } else if (result) {
+        // Обновляем значение в состоянии
+        setStarted(result);
+      }
+    });
+  }, []);
 
   return (
     <>
       <div>
-        <a  target="_blank">
+        <a target="_blank">
           <img src={dicelogo} className="logo" alt="Dice logo" />
         </a>
       </div>
       <h1>DICE Time Tracker</h1>
       <div className="card">
-      WebApp.CloudStorage.getItem("started_at")
+        {started}
       </div>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
