41a8e3d6157876c2aaa7336e75b6fec53ff3a5e5
Lev Tomatny <mastergefest@gmail.com>
2023-10-30 03:22:31 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
+import dicelogo from './assets/dice.png'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
@@ -18,14 +16,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a  target="_blank">
+          <img src={dicelogo} className="logo" alt="Dice logo" />
         </a>
       </div>
       <h1>TWA + Vite + React</h1>
